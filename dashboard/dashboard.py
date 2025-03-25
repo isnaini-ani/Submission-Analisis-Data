f46065aea9e637da6833ad82268913f42df3e99c
isnaini-ani <isnaini.anicn@gmail.com>
2025-03-25 12:58:09 +0700

    mengubah tampilan transaksi dan order

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+import matplotlib.dates as mdates
 sns.set(style='dark')
 
 #untuk menyiapkan orders_daily_df
